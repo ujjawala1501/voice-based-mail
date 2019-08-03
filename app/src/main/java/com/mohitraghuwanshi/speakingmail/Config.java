fc9cbad35d6cf3597714b8d6f19ab68f7fc3f799
Your Name <you@example.com>
2019-08-03 12:31:06 +0530

    voice based mail-android

---
+++ app/src/main/java/com/mohitraghuwanshi/speakingmail/Config.java
@@ -0,0 +1,6 @@
+package com.mohitraghuwanshi.speakingmail;
+
+public class Config {
+    public static  String EMAIL ="gaming.guy.mohit@gmail.com";  //gmail address
+    public static  String PASSWORD ="xXxyo!soundsgudhaha"; //password
+}
